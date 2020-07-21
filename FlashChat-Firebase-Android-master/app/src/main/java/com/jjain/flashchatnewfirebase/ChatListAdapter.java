e286ad14aa763e99854283f9239ada65a5ffb2b6
jeesu1729 <jeesujain02oct@gmail.com>
2020-07-21 16:42:00 +0530

    first commit
    first commmit

---
+++ FlashChat-Firebase-Android-master/app/src/main/java/com/jjain/flashchatnewfirebase/ChatListAdapter.java
@@ -0,0 +1,162 @@
+package com.jjain.flashchatnewfirebase;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+
+public class ChatListAdapter extends BaseAdapter {
+
+    private Activity mActivity;
+    private DatabaseReference mDatabaseReference;
+    private String mDisplayName;
+    private ArrayList<DataSnapshot> mSnapshotList;
+    private ChildEventListener mListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            mSnapshotList.add(dataSnapshot);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    };
+
+
+
+    @Override
+    public int getCount() {
+        return mSnapshotList.size();
+    }
+
+    @Override
+    public InstantMessage getItem(int position) {
+        DataSnapshot snapshot = mSnapshotList.get(position);
+        return snapshot.getValue(InstantMessage.class);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            assert inflater != null;
+            convertView = inflater.inflate(R.layout.chat_msg_row, parent, false);
+
+            final ViewHolder holder = new ViewHolder();
+            holder.authorName = convertView.findViewById(R.id.author);
+            holder.body = convertView.findViewById(R.id.message);
+            holder.params = (LinearLayout.LayoutParams) holder.authorName.getLayoutParams();
+            convertView.setTag(holder);
+
+        }
+
+        final InstantMessage message = getItem(position);
+        final ViewHolder holder = (ViewHolder) convertView.getTag();
+        Log.d("FlashChat", "getView: "+mDisplayName);
+        boolean isMe = message.getAuthor().equals(mDisplayName);
+
+        setChatRowAppearance(isMe, holder);
+
+        String author = message.getAuthor();
+        holder.authorName.setText(author);
+
+        String msg = message.getMessage();
+        holder.body.setText(msg);
+
+        return convertView;
+    }
+
+    public ChatListAdapter(Activity activity, DatabaseReference ref, String name) {
+
+        mActivity = activity;
+        mDisplayName = name;
+        Log.d("FlashChat", "ChatListAdapter: " + name);
+        // common error: typo in the db location. Needs to match what's in MainChatActivity.
+        mDatabaseReference = ref.child("messages");
+        mDatabaseReference.addChildEventListener(mListener);
+
+        mSnapshotList = new ArrayList<>();
+    }
+    private static class ViewHolder{
+        TextView authorName;
+        TextView body;
+        LinearLayout.LayoutParams params;
+    }
+    private void setChatRowAppearance(boolean isItMe, ViewHolder holder) {
+
+        if (isItMe)
+        {
+
+            holder.params.gravity = Gravity.END;
+            holder.authorName.setTextColor(Color.parseColor("#183A37"));
+
+            // If you want to use colours from colors.xml
+            // int colourAsARGB = ContextCompat.getColor(mActivity.getApplicationContext(), R.color.yellow);
+            // holder.authorName.setTextColor(colourAsARGB);
+
+            holder.body.setBackgroundResource(R.drawable.bubble2);
+            holder.body.setAlpha((float) 1);
+            holder.body.setTextColor(Color.parseColor("#2C6560"));
+
+        }
+
+        else
+        {
+            holder.params.gravity = Gravity.START;
+            holder.authorName.setTextColor(Color.parseColor("#192251"));
+            holder.body.setBackgroundResource(R.drawable.bubble1);
+            holder.body.setAlpha((float) 1);
+            holder.body.setTextColor(Color.parseColor("#1D377A"));
+        }
+
+        holder.authorName.setLayoutParams(holder.params);
+        holder.body.setLayoutParams(holder.params);
+
+    }
+
+
+    void cleanup() {
+
+        mDatabaseReference.removeEventListener(mListener);
+    }
+
+}
---
+++ FlashChat-Firebase-Android-master/app/src/main/java/com/jjain/flashchatnewfirebase/splash.java
@@ -0,0 +1,24 @@
+package com.jjain.flashchatnewfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(splash.this,LoginActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },3000);
+    }
+}
